c3992365450a4277343ec0c02775d8f99181ad92
Thiago <enseadadascores@gmail.com>
2026-02-24 22:39:17 -0300

    boscariol

--- boscariolComponents.js
+++ boscariolComponents.js
@@ -34,6 +34,39 @@ $menuMobileBt.addEventListener("click",()=>{
     }
 
 });
+//image/project0.jpeg
+ref(".gallery");
+ref(".galleryLeftBt")
+ref(".galleryRightBt")
+
+let galleryList = [{ref:"project",max:10},{ref:"drill",max:5}];
+
+function gallery(){
+
+    let galleryCounter = 0;
+
+    function galleryChangeIMage(index){
+        if(galleryCounter>0&&galleryCounter<galleryList[index].max){
+        }else if(galleryCounter>=galleryList[index].max){
+            galleryCounter=0;
+        }else if(galleryCounter<0){
+            galleryCounter=galleryList[index].max-1;
+        };
+        __gallery[index].style.backgroundImage=`linear-gradient(var(--imgCover)),url(image/${galleryList[index].ref}${galleryCounter}.jpeg)`;
+    }
+
+    for(let index in galleryList){
+
+        __galleryLeftBt[index].addEventListener("click",()=>{galleryCounter--;galleryChangeIMage(index)});
+        __galleryRightBt[index].addEventListener("click",()=>{galleryCounter++;galleryChangeIMage(index)});
+    }
+
+};
+
+gallery()
+
+console.log(__gallery)
+
 
 },2000);
 })();
